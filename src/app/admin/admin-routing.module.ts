7a0e38589ed34fbadbceb2b4cd7d2bde3c6a4a90
AhmedAlazaizeh <azaizeh44@gmail.com>
2021-12-02 14:17:38 +0200

    a

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SystemComponent } from './system/system.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'employee',
+    component: EmployeeComponent
+  },
+  {
+    path: 'feedback',
+    component: FeedbackComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'system',
+    component: SystemComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
