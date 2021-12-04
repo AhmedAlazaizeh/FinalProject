a4f76e4a59e737d0924acaa73ef7fa05458d5920
AhmedAlazaizeh <azaizeh44@gmail.com>
2021-12-04 16:34:58 +0200

    Ahmed

--- src/app/influncer/influncer.module.ts
+++ src/app/influncer/influncer.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InfluncerRoutingModule } from './influncer-routing.module';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ProfileComponent
+  ],
   imports: [
     CommonModule,
     InfluncerRoutingModule
