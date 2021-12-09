7dec13e9b98de362469c69a32f58e1344710cc9d
AhmedAlazaizeh <azaizeh44@gmail.com>
2021-12-09 19:42:54 +0200

    Ahmed

--- src/app/services/admin/home.service.ts
+++ src/app/services/admin/home.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
 
-  constructor() { }
+  countOfEmployees: any = {}
+  countOfCustomers: any = {}
+  countOfInfluncers: any = {}
+  countOfFeedback: any = {}
+  constructor( private http: HttpClient) { }
+
+  getCountOfEmployees(){
+    this.http.get("https://localhost:44309/api/User/countOfEmployees").subscribe((Response: any)=>{this.countOfEmployees=Response})
+  }
+
+  getCountOfCustomers(){
+    this.http.get("https://localhost:44309/api/User/countOfCustomers").subscribe((Response: any)=>{this.countOfCustomers=Response})
+  }
+
+  getCountOfInfluncers(){
+    this.http.get("https://localhost:44309/api/User/countOfInfluncers").subscribe((Response: any)=>{this.countOfInfluncers=Response})
+  }
+
+
+  getCountOfFeedback(){
+    this.http.get("https://localhost:44309/api/Feedback/countOfFeedback").subscribe((Response: any)=>{this.countOfFeedback=Response})
+  }
 }
--- src/app/services/auth/login.service.ts
+++ src/app/services/auth/login.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
   token: string | any
+  data: any = [{}]
+
 
   constructor(private http:HttpClient) { }
 
@@ -22,5 +25,11 @@ export class LoginService {
 
     this.http.post("https://localhost:44309/api/JWT/auth", form, requestOptions).subscribe((Response: any)=>{this.token=Response})
     console.log(this.token)
+  }
+
+  getUserByUsername(username: string){
+
+    this.http.get("https://localhost:44309/api/User/getUserByUsername/" + username).subscribe((Response: any)=>{this.data=Response})
+
   }
 }
