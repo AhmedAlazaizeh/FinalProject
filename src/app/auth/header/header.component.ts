d80fa5a7d932a672a7c8b3b4e09c0833967424ea
AhmedAlazaizeh <azaizeh44@gmail.com>
2021-12-15 18:30:57 +0200

    Ahmed

--- src/app/auth/header/header.component.ts
+++ src/app/auth/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { HeaderService } from 'src/app/services/customer/header.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  usernameReturned: any
+
+  username = localStorage.getItem("username")
+  constructor(private router: Router, public headerService: HeaderService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    var ID = localStorage.getItem("userID")
+    console.log(ID)
+    this.getUsername();
+    this.getCartCount(ID)
   }
 
+  logOut(){
+    localStorage.clear()
+    this.toastr.error("Logged Out!")
+    this.router.navigate(["/Auth"])
+  }
+
+  getUsername(){
+    this.headerService.getUserByUsername(this.username!)
+  }
+
+  getCartCount(ID: any){
+    this.headerService.getCartCount(ID)
+  }
 }
