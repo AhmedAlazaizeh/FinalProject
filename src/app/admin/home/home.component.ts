aba1035d30d8a99d92dd3bb645ca1d6cffc86680
AhmedAlazaizeh <azaizeh44@gmail.com>
2021-12-17 20:09:05 +0200

    Ahmed

--- src/app/admin/home/home.component.ts
+++ src/app/admin/home/home.component.ts
@@ -67,30 +67,22 @@ export class HomeComponent implements OnInit {
   }
 
   exportToExcel(tableID: string){
-    /* pass here the table id */
     let element = document.getElementById(tableID);
     const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
-
-    /* generate workbook and add the worksheet */
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-
-    /* save to file */
     XLSX.writeFile(wb, tableID + ".xlsx");
   }
 
   exportToPDF(tableID: string){
     let data = document.getElementById(tableID)
     html2canvas(data!).then(canvas => {
-
       let fileWidth = 208;
       let fileHeight = canvas.height * fileWidth / canvas.width;
-
       const FILEURI = canvas.toDataURL('image/png')
       let PDF = new jspdf('p', 'mm', 'a4');
       let position = 0;
       PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight)
-
       PDF.save(tableID +'.pdf');
   });
   }
