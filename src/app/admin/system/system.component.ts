a2435f327b7d51489ef4d9e6f7fcd6a35ffb55f2
AhmedAlazaizeh <azaizeh44@gmail.com>
2021-12-17 23:24:52 +0200

    Ahmed

--- src/app/admin/system/system.component.ts
+++ src/app/admin/system/system.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SystemService } from 'src/app/services/admin/system.service';
+import * as XLSX from 'xlsx';
+import jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-system',
@@ -16,5 +19,26 @@ export class SystemComponent implements OnInit {
 
   getSystem(){
     this.systemService.getAllSystem()
+  }
+
+  exportToExcel(tableID: string){
+    let element = document.getElementById(tableID);
+    const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    XLSX.writeFile(wb, tableID + ".xlsx");
+  }
+
+  exportToPDF(tableID: string){
+    let data = document.getElementById(tableID)
+    html2canvas(data!).then(canvas => {
+      let fileWidth = 208;
+      let fileHeight = canvas.height * fileWidth / canvas.width;
+      const FILEURI = canvas.toDataURL('image/png')
+      let PDF = new jspdf('p', 'mm', 'a4');
+      let position = 0;
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight)
+      PDF.save(tableID +'.pdf');
+  });
   }
 }
