742e86ea79425922b9eb2140820d67b1f72d90b4
AhmedAlazaizeh <azaizeh44@gmail.com>
2021-12-19 17:43:36 +0200

    Ahmed

--- src/app/customer/orders/orders.component.ts
+++ src/app/customer/orders/orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from 'src/app/services/customer/orders.service';
 
 @Component({
   selector: 'app-orders',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+  constructor(public ordersService: OrdersService) { }
 
   ngOnInit(): void {
+    var ID = localStorage.getItem("userID")
+    this.getUserOrderList(ID)
+    this.sumOfMyOrders(ID)
   }
 
+  getUserOrderList(ID: any){
+    this.ordersService.getOrderList(ID)
+  }
+
+  sumOfMyOrders(ID: any){
+    this.ordersService.sumOfMyOrders(ID)
+  }
 }
--- src/app/services/customer/orders.service.ts
+++ src/app/services/customer/orders.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,16 @@ import { Injectable } from '@angular/core';
 })
 export class OrdersService {
 
-  constructor() { }
+  userOrderList: any = [{}]
+  sumOfMyOrdersData: any = {}
+
+  constructor(public http: HttpClient) { }
+
+  getOrderList(ID: number){
+    this.http.get("https://localhost:44309/api/Order/orderList/" + ID).subscribe((Response: any)=>{this.userOrderList=Response})
+  }
+
+  sumOfMyOrders(ID: number){
+    this.http.get("https://localhost:44309/api/Order/sumOfMyOrders/" + ID).subscribe((Response: any)=>{this.sumOfMyOrdersData=Response})
+  }
 }
