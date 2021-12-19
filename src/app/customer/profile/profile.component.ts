49b86e41341c08038702d3209295f91a009e0217
AhmedAlazaizeh <azaizeh44@gmail.com>
2021-12-19 23:46:47 +0200

    Ahmed

--- src/app/customer/profile/profile.component.ts
+++ src/app/customer/profile/profile.component.ts
@@ -11,6 +11,19 @@ import { DialogComponent } from 'src/app/dialog/dialog.component';
 })
 export class ProfileComponent implements OnInit {
 
+  userID: number = 0
+  fName: string = "aa"
+  lName: string = "bb"
+  email: string = ""
+  phoneNumber: string = ""
+  username: string = ""
+  password: string = ""
+  roleID: number = 0
+  salary: number = 0
+  longitude: string = ""
+  latitude: string = ""
+  employmentDate: any
+
   loggedInUserID: number = Number(localStorage.getItem("userID"))
 
   constructor(public profileService: ProfileService) {}
@@ -18,14 +31,28 @@ export class ProfileComponent implements OnInit {
   ngOnInit(): void {
     var ID = localStorage.getItem("userID")
     this.getUserByID(ID)
+
+    this.userID = Number(localStorage.getItem("userID"))!
+    this.fName = localStorage.getItem("fName")!
+    this.lName = localStorage.getItem("lName")!
+    this.email = localStorage.getItem("email")!
+    this.phoneNumber = localStorage.getItem("phoneNumber")!
+    this.username = localStorage.getItem("username")!
+    this.password = localStorage.getItem("password")!
+    this.salary = Number(localStorage.getItem("salary"))!
+    this.employmentDate = localStorage.getItem("employmentDate")!
+    this.longitude = localStorage.getItem("longitude")!
+    this.latitude = localStorage.getItem("latitude")!
+    this.roleID = Number(localStorage.getItem("fName"))!
+
   }
 
   getUserByID(ID: any){
     this.profileService.getUserByID(ID)
   }
 
-  updateUser(data: any){
-    this.profileService.updateUser(data)
-    window.location.reload();
+  updateUser(){
+    this.profileService.updateUser(this.userID, this.fName, this.lName, this.email, this.phoneNumber, this.username, this.password, this.roleID, this.longitude, this.latitude, this.employmentDate)
+    //window.location.reload();
   }
 }
--- src/app/services/customer/profile.service.ts
+++ src/app/services/customer/profile.service.ts
@@ -7,26 +7,40 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class ProfileService {
 
-  data: any = [{}]
+  data: any = {}
   data1: any = [{}]
   constructor(private http:HttpClient, private toastr: ToastrService) { }
 
   getUserByID(ID: number){
-    this.http.get("https://localhost:44309/api/User/getUser/" + ID).subscribe((Response:any)=>{this.data=Response})
+    this.http.get("https://localhost:44309/api/User/getUser/" + ID).subscribe((Response:any)=>{this.data=Response
+
+    localStorage.setItem("userID", Response["userID"])
+    localStorage.setItem("fName", Response["fName"])
+    localStorage.setItem("lName", Response["lName"])
+    localStorage.setItem("email", Response["email"])
+    localStorage.setItem("phoneNumber", Response["phoneNumber"])
+    localStorage.setItem("username", Response["username"])
+    localStorage.setItem("password", Response["password"])
+    localStorage.setItem("longitude", Response["longitude"])
+    localStorage.setItem("latitude", Response["latitude"])
+    localStorage.setItem("employmentDate", Response["employmentDate"])
+    localStorage.setItem("roleID", Response["roleID"])
+    localStorage.setItem("salary", Response["salary"])
+  })
   }
 
-  updateUser(data1: any){
-    this.http.put('https://localhost:44309/api/User/Update',data1).subscribe((result)=>{
+  updateUser(userID: number, fName: string, lName: string, email: string, phoneNumber: string, username: string, password: string, roleID: number, latitude: string, longitude: string, employmentDate: Date){
+
+    this.http.put('https://localhost:44309/api/User/UpdateUser', {userID: userID, fName: fName, lName: lName, email: email, phoneNumber: phoneNumber, username: username, password: password, roleID: roleID, longitude: longitude, latitude: latitude, employmentDate: employmentDate}).subscribe((result)=>{
 
     this.toastr.success('Info Updated');
-     console.log(data1);
 
    },err=>{
 
      console.log(err);
-     console.log(data1);
      this.toastr.error(err);
 
       })
   }
+
 }
