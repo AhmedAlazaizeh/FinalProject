fea29c733a3c58371ac5115ae7abdb20e671df03
Qaddoummi <Mohammadqaddoumi72@gmail.com>
2021-12-19 10:32:04 +0200

    Accountant

--- src/app/accountant/home/home.component.ts
+++ src/app/accountant/home/home.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { HomeService } from 'src/app/services/accountant/home.service';
+import * as XLSX from 'xlsx';
+import jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  username = localStorage.getItem("username")
+
+  constructor(public homeService: HomeService) { }
 
   ngOnInit(): void {
+    this.getCountOfEmployees()
+    this.getCountOfCustomers()
+    this.getCountOfInfluncers()
+    this.getCountOfFeedback()
+    this.getSumOfSales()
+    this.getSumOfRevune()
+    this.getOrderList()
+    this.getFinancialList()
+    this.getCountOfOrders()
+    this.getCountOfActiveProducts()
+
+    var ID = localStorage.getItem("userID")
+    this.getUsername();
   }
 
+  getCountOfEmployees(){
+  this.homeService.getCountOfEmployees()
+  }
+
+  getCountOfCustomers(){
+    this.homeService.getCountOfCustomers()
+  }
+
+  getCountOfInfluncers(){
+    this.homeService.getCountOfInfluncers()
+  }
+
+  getCountOfFeedback(){
+    this.homeService.getCountOfFeedback()
+  }
+
+  getOrderList(){
+    this.homeService.getOrderLsit()
+  }
+
+  getFinancialList(){
+    this.homeService.getFinancialList()
+  }
+
+  getSumOfSales(){
+    this.homeService.getSumOfSales()
+  }
+
+  getSumOfRevune(){
+    this.homeService.getSumOfRevune()
+  }
+
+  getCountOfOrders(){
+    this.homeService.getCountOfOrders()
+  }
+
+  getCountOfActiveProducts(){
+    this.homeService.getCountOfActiveProducts()
+  }
+
+  exportToExcel(tableID: string){
+    let element = document.getElementById(tableID);
+    const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    XLSX.writeFile(wb, tableID + ".xlsx");
+  }
+
+  exportToPDF(tableID: string){
+    let data = document.getElementById(tableID)
+    html2canvas(data!).then(canvas => {
+      let fileWidth = 208;
+      let fileHeight = canvas.height * fileWidth / canvas.width;
+      const FILEURI = canvas.toDataURL('image/png')
+      let PDF = new jspdf('p', 'mm', 'a4');
+      let position = 0;
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight)
+      PDF.save(tableID +'.pdf');
+  });
+  }
+
+  getUsername(){
+    this.homeService.getUserByUsername(this.username!)
+  }
 }
--- src/app/services/accountant/home.service.ts
+++ src/app/services/accountant/home.service.ts
@@ -1,9 +1,78 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
 
-  constructor() { }
+  countOfEmployees: any = {}
+  countOfCustomers: any = {}
+  countOfInfluncers: any = {}
+  countOfFeedback: any = {}
+  sumOfSales: any = {}
+  sumOfRevune: any = {}
+  countOfOrders: any = {}
+  countOfActiveProducts: any = {}
+  orderListData: any = [{}]
+  financialData: any = [{}]
+
+  data1: any | string = [{}]
+
+  un = localStorage.getItem("username")
+  username = [{"username": this.un}]
+
+  constructor( private http: HttpClient) { }
+
+  getCountOfEmployees(){
+    this.http.get("https://localhost:44309/api/User/countOfEmployees").subscribe((Response: any)=>{this.countOfEmployees=Response})
+  }
+
+  getCountOfCustomers(){
+    this.http.get("https://localhost:44309/api/User/countOfCustomers").subscribe((Response: any)=>{this.countOfCustomers=Response})
+  }
+
+  getCountOfInfluncers(){
+    this.http.get("https://localhost:44309/api/User/countOfInfluncers").subscribe((Response: any)=>{this.countOfInfluncers=Response})
+  }
+
+
+  getCountOfFeedback(){
+    this.http.get("https://localhost:44309/api/Feedback/countOfFeedback").subscribe((Response: any)=>{this.countOfFeedback=Response})
+  }
+
+  getSumOfSales(){
+    this.http.get("https://localhost:44309/api/Order/sumOfSales").subscribe((Response: any)=>{this.sumOfSales=Response})
+  }
+
+  getSumOfRevune(){
+    this.http.get("https://localhost:44309/api/Order/sumOfRevune").subscribe((Response: any)=>{
+      this.sumOfRevune["sumOfRevune"] = Math.round(Response["sumOfRevune"] * 100) / 100
+  })
+  }
+
+  getOrderLsit(){
+    this.http.get("https://localhost:44309/api/Order/ordersList").subscribe((Response: any)=>{this.orderListData=Response})
+  }
+
+  getFinancialList(){
+    this.http.get("https://localhost:44309/api/User/GetFinancial").subscribe((Response: any)=>{this.financialData=Response})
+  }
+
+  getCountOfOrders(){
+    this.http.get("https://localhost:44309/api/Order/countOfOrders").subscribe((Response: any)=>{this.countOfOrders=Response})
+  }
+
+  getCountOfActiveProducts(){
+    this.http.get("https://localhost:44309/api/Product/countOfAvailableProducts").subscribe((Response: any)=>{this.countOfActiveProducts=Response})
+  }
+
+  getUserByUsername(username: string){
+    if (username != null) {
+      this.http.get("https://localhost:44309/api/User/getUserByUsername/" + username).subscribe((Response: any)=>{this.data1=Response})
+    }else{
+      this.data1 = null
+    }
+
+  }
 }
