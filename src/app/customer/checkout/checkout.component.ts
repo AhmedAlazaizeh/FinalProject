d7a8bb5c10006c779f06252ef3629b1bc82a8f8a
AhmedAlazaizeh <azaizeh44@gmail.com>
2021-12-19 13:39:25 +0200

    Ahmed

--- src/app/customer/checkout/checkout.component.ts
+++ src/app/customer/checkout/checkout.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { Toast, ToastrService } from 'ngx-toastr';
+import { CheckoutService } from 'src/app/services/customer/checkout.service';
 
 @Component({
   selector: 'app-checkout',
@@ -7,9 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor() { }
+x: Number = Number(localStorage.getItem("sumOfCart"))
+
+  paymentForm: FormGroup = new FormGroup({
+    cardNumber: new FormControl('', [Validators.required]),
+    CVV: new FormControl('', [Validators.required]),
+    expDate: new FormControl('', [Validators.required]),
+    cartSum: new FormControl(this.x)
+  })
+
+  username = localStorage.getItem("username")
+
+  constructor(public checkoutService: CheckoutService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    var ID = localStorage.getItem("userID")
+    this.getUsername();
+    this.getCartList(ID)
+    this.getSumOfCart(ID)
+    localStorage.setItem("paymentButtonFlag", "0")
   }
 
+  getCartList(ID: any){
+    this.checkoutService.getCartList(ID)
+  }
+
+  getSumOfCart(ID: any){
+    this.checkoutService.getSumOfCart(ID)
+  }
+
+  pay(){
+    this.checkoutService.pay(this.paymentForm.value)
+    console.log(this.paymentForm.value)
+
+    if (localStorage.getItem("paymentButtonFlag") == "1") {
+      if (localStorage.getItem("paymentFlag") == "True") {
+        alert("payed!")
+        this.toastr.success("Payed Successfuly", "Thank You!")
+      }else{
+        alert("not payed!")
+        this.toastr.error("There Was An Error!","Check Your Card Info Or Balance")
+      }
+    }
+    localStorage.setItem("paymentButtonFlag", "1")
+  }
+
+  getUsername(){
+    this.checkoutService.getUserByUsername(this.username!)
+  }
 }
--- src/app/services/customer/checkout.service.ts
+++ src/app/services/customer/checkout.service.ts
@@ -1,9 +1,55 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutService {
 
-  constructor() { }
+  cartList: any = [{}]
+  sumOfCart: any = {}
+  data1: any | string = [{}]
+  paymentResponse: any = {}
+
+  constructor(private http: HttpClient) { }
+
+  getCartList(ID: number){
+    this.http.get("https://localhost:44309/api/Order/cartList/" + ID).subscribe((Response: any)=>{this.cartList=Response})
+  }
+
+  getSumOfCart(ID: number){
+    this.http.get("https://localhost:44309/api/Order/sumOfCart/" + ID).subscribe((Response: any)=>{this.sumOfCart=Response
+    localStorage.setItem("sumOfCart", Response["sumOfCart"])
+    })
+
+  }
+
+  getUserByUsername(username: string){
+    if (username != null) {
+      this.http.get("https://localhost:44309/api/User/getUserByUsername/" + username).subscribe((Response: any)=>{this.data1=Response})
+    }else{
+      this.data1 = null
+    }
+  }
+
+  async pay(form: FormGroup){
+
+    const headerDict = {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    };
+
+    const requestOptions = {
+      headers: new HttpHeaders(headerDict),
+    };
+
+      this.http.post("https://localhost:44309/api/BankAccount/Pay", form, requestOptions).subscribe((Response: any)=>{this.paymentResponse=Response
+      localStorage.setItem("paymentFlag", "True")
+      }
+      ,err=>{
+        this.paymentResponse = null
+        localStorage.setItem("paymentFlag", "False")
+      })
+  }
 }
